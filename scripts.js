3dffe93eb8228c6384ab75e03b991baf93472ef2
Pablo Velasquez <pevb97@gmail.com>
2024-10-22 21:52:13 -0500

    Refactorizacion de codigo mediante cambio de video a input file

--- scripts.js
+++ scripts.js
@@ -1,80 +1,37 @@
-const video = document.getElementById('video');
+const inputQRFile = document.getElementById('InputCamera');
 const output = document.getElementById('output');
 const restartBtn = document.getElementById('restartBtn');
 
 let scanningActive = true; // Variable para controlar si la lectura está activa o no
 
-// Obtener acceso a la cámara
-navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-    .then(stream => {
-        video.srcObject = stream;
-    })
-    .catch(err => {
-        console.error("Error al acceder a la cámara: " + err);
-    });
+inputQRFile.addEventListener('change', (event) => {
+    const qrFile = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        let img = new Image();
+        img.src = event.target.result;
 
-// Decodificar el QR de la cámara en tiempo real
-video.addEventListener('play', () => {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    
-    function scanQRCode() {
-        if (!scanningActive) {
-            alert(`Escaneo activado: ${scanningActive}`);
-            return;  // Si la lectura está pausada, no seguir escaneando
-        }
-    
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        const qrCode = jsQR(imageData.data, canvas.width, canvas.height);
-    
-        if (qrCode) {
-            // Mostrar los datos del QR
-            const qrData = qrCode.data;
-            if (qrData.startsWith("WIFI:")) {
-                alert(`Escaneo activado: ${scanningActive}`);
-                output.textContent = "Código QR de red Wi-Fi detectado.";
-                extractWifiInfo(qrData);
-                scanningActive = false;  // Pausar la lectura
-                alert(`Escaneo activado: ${scanningActive}`);
+        img.onload = function (event) {
+            let canvas = document.createElement('canvas');
+            canvas.style.display = 'none';
+            canvas.width = img.width;
+            canvas.height = img.height;
+            let context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0, canvas.width, canvas.height);
+            let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            let qrCode = jsQR(imageData.data, canvas.width, canvas.height);
+            if (qrCode) {
+
+                output.textContent = getInfoWiFiQR(qrCode.data);
             } else {
-                output.textContent = "Código QR detectado, pero no es de una red Wi-Fi.";
+                output.textContent = "No se detectó ningún código QR.";
             }
-        } else {
-            output.textContent  = "No se ha detectado un código QR.";
-        }
-    
-        if (scanningActive) {
-            requestAnimationFrame(scanQRCode); // Seguir escaneando si está activo
-        }
-    }
-    scanQRCode();
+        };
+    };
+    reader.readAsDataURL(qrFile);
 });
 
-// Función para extraer la información de la red Wi-Fi del código QR
-function extractWifiInfo(qrData) {
-    const wifiData = qrData.slice(5).split(';');
-    let wifiInfo = {};
-
-    wifiData.forEach(pair => {
-        if (pair.includes(':')) {
-            const [key, value] = pair.split(':', 2);
-            wifiInfo[key] = value;
-        }
-    });
-
-    output.textContent = `Nombre de la red: ${wifiInfo['S']}\nEncriptación: ${wifiInfo['T']} \nContraseña: ${wifiInfo['P']}`;
-}
-
-// Función para reiniciar el escaneo
-function restartScanner() {
-    alert("Botn de reseteo oprimido");
-    alert(`Escaneo activado: ${scanningActive}`);
-    scanningActive = true;
-    alert(`Escaneo activado: ${scanningActive}`);
-    output.textContent = "";
-    requestAnimationFrame(scanQRCode);  // Reiniciar el escaneo
+function getInfoWiFiQR(data) {
+    let [ nombreRed, clave ] = data.split(';').slice(1, 3).map(text => text.replace(/^[a-zA-Z]:/, ''));
+    return `Nombre Red: ${nombreRed}; Clave: ${clave}`;
 }
